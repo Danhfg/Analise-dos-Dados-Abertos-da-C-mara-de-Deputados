6b8d2ff1fa32d2e283d6b1840791d57c06ddab71
Danhfg <danielhenriquefg@gmail.com>
2018-09-23 19:06:10 -0300

    Update questions.py

--- questions.py
+++ questions.py
@@ -38,16 +38,23 @@ df_2015.head(4)""")
   def hint():
     print("Importe o pandas como pd, depois utilize os métodos \033[1mpandas.read_cs()\033[0m e \033[1mdataFrame.head()\033[0m")
   def check():
-    if (str(df_2017) != str(df7)):
-      print("\033[91mErrado\033[0m: a variável df_2017 não foi criada corretamente, certifique-se de utilizar o separado ';' e o index a coluna 'idProposicao'")
-      return None
-    if (str(df_2016) != str(df6)):
-      print("\033[91mErrado\033[0m: a variável df_2016 não foi criada corretamente, certifique-se de utilizar o separado ';' e o index a coluna 'idProposicao'")
-      return None
-    if (str(df_2015) != str(df5)):
-      print("\033[91mErrado\033[0m: a variável df_2015 não foi criada corretamente, certifique-se de utilizar o separado ';' e o index a coluna 'idProposicao'")
-      return None
-    print("\033[92mCorreto\033[0m: DataFrames criados corretamente")
+    try:
+      df7 = pd.read_csv('https://dadosabertos.camara.leg.br/arquivos/proposicoesAutores/csv/proposicoesAutores-2017.csv', sep=';', index_col='idProposicao')
+      df6 = pd.read_csv('https://dadosabertos.camara.leg.br/arquivos/proposicoesAutores/csv/proposicoesAutores-2016.csv', sep=';', index_col='idProposicao')
+      df5 = pd.read_csv('https://dadosabertos.camara.leg.br/arquivos/proposicoesAutores/csv/proposicoesAutores-2015.csv', sep=';', index_col='idProposicao')
+      if (str(df_2017) != str(df7)):
+        print("\033[91mErrado\033[0m: a variável df_2017 não foi criada corretamente, certifique-se de utilizar o separador ';' e o index a coluna 'idProposicao'")
+        return None
+      if (str(df_2016) != str(df6)):
+        print("\033[91mErrado\033[0m: a variável df_2016 não foi criada corretamente, certifique-se de utilizar o separador ';' e o index a coluna 'idProposicao'")
+        return None
+      if (str(df_2015) != str(df5)):
+        print("\033[91mErrado\033[0m: a variável df_2015 não foi criada corretamente, certifique-se de utilizar o separador ';' e o index a coluna 'idProposicao'")
+        return None
+      print("\033[92mCorreto\033[0m: DataFrames criados corretamente")
+    except ValueError:
+      print("\033[91mErrado\033[0m: as variável não foram criadas corretamente, certifique-se de utilizar o separador ';' e o index a coluna 'idProposicao'")
+
     
 q2 = questao2
 
@@ -71,10 +78,14 @@ class questao4:
   def hint():
     print("Utilize o método \033[1mdataFrame.append(dataFrame)\033[0m")
   def check():
-    if( str(df) == str(dfg11) or str(df) == str(dfg12) or str(df) == str(dfg21) or str(df) == str(dfg22) or str(df) == str(dfg31) or str(df) == str(dfg32) ):
-      print("\033[92mCorreto\033[0m: DataFrames  df criado corretamente")
-    else:
-      print("\033[91mErrado\033[0m: a variável df não foi criada corretamente, certifique-se de juntar todos os 3 dataFrames")
+    try:
+      if( str(df) == str(dfg11) or str(df) == str(dfg12) or str(df) == str(dfg21) or str(df) == str(dfg22) or str(df) == str(dfg31) or str(df) == str(dfg32) ):
+        print("\033[92mCorreto\033[0m: DataFrame df criado corretamente")
+      else:
+        print("\033[91mErrado\033[0m: a variável df não foi criada corretamente, certifique-se de juntar todos os 3 dataFrames (lembre-se de seguir a ordem 2015, 2016, 2017) )!")
+    except ValueError:
+      print("\033[91mErrado\033[0m: a variável df não foi criada corretamente, certifique-se de juntar todos os 3 dataFrames (lembre-se de seguir a ordem 2015, 2016, 2017) )!")
+
     
 q4 = questao4
 
@@ -85,10 +96,14 @@ class questao5:
   def hint():
     print("Você pode tentar utilizar a seguinte sintaxe: novo_df = nome_df[nome_df['coluna'] == 'opção']")
   def check():
-    if (str(df_rn_teste) == str(df_rn)):
-      print("\033[92mCorreto\033[0m: DataFrame df_rn criado corretamente")
-    else:
+    try:
+      if (str(df_rn_teste) == str(df_rn)):
+        print("\033[92mCorreto\033[0m: DataFrame df_rn criado corretamente")
+      else:
+        print("\033[91mErrado\033[0m: a variável df_rn não foi criada corretamente (verifique os nomes das colunas para identificar a correta e selecionar 'RN').")
+    except ValueError:
       print("\033[91mErrado\033[0m: a variável df_rn não foi criada corretamente (verifique os nomes das colunas para identificar a correta e selecionar 'RN').")
+
 
 q5 = questao5
 
@@ -99,10 +114,14 @@ class questao6:
   def hint():
     print("O método df.value_counts pode ser útil!")
   def check():
-    if (str(prop_number_teste) == str(prop_number)):
-      print("\033[92mCorreto\033[0m: DataFrame df_rn criado corretamente!")
-    else:
-      print("\033[91mErrado\033[0m: ")
+    try:
+      if (str(prop_number_teste) == str(prop_number)):
+        print("\033[92mCorreto\033[0m: DataFrame df_rn criado corretamente!")
+      else:
+        print("\033[91mErrado\033[0m: df_rn não foi criado corretamente!")
+    except ValueError:
+      print("\033[91mErrado\033[0m: df_rn não foi criado corretamente!")
+
 
 q6 = questao6
 
@@ -124,12 +143,15 @@ print(qnt_brasil)""")
   def hint():
     print("O método dataFame.count() mostra quantos elementos possui em uma determinada coluna, com isso, basta dividir\no primeiro elemento de series.value_counts() pela quantidade de elementros na coluna 'uriProposicao'")
   def check():
-    if (porcen_rn_teste != porcen_rn):
-      print("\033[91mErrado\033[0m: porcen_rn não foi criado corretamente!")
-    elif (porcen_br_teste != porcen_br):
-      print("\033[91mErrado\033[0m: porcen_br não foi criado corretamente!")
-    else:
-      print("\033[92mCorreto\033[0m: Valores calculados de forma correta!")
+    try:
+      if (porcen_rn_teste != porcen_rn):
+        print("\033[91mErrado\033[0m: porcen_rn não foi criado corretamente!")
+      elif (porcen_br_teste != porcen_br):
+        print("\033[91mErrado\033[0m: porcen_br não foi criado corretamente!")
+      else:
+        print("\033[92mCorreto\033[0m: Valores calculados de forma correta!")
+    except ValueError:
+      print("\033[91mErrado\033[0m: As variáveis não foram criadas corretamente!")
       
 q7 = questao7
 
@@ -143,16 +165,20 @@ print(prop_number[ prop_number > media_prop ] )""")
   def hint():
     print("Utilize o método mean() em dataFrame.value_counts()")
   def check():
-    if(media_prop_teste == media_prop):
-      print("\033[92mCorreto\033[0m: Valor calculado de forma correta!")
-    else:
+    try:
+      if(media_prop_teste == media_prop):
+        print("\033[92mCorreto\033[0m: Valor calculado de forma correta!")
+      else:
+        print("\033[91mErrado\033[0m: media_prop não foi criado corretamente!")
+    except ValueError:
       print("\033[91mErrado\033[0m: media_prop não foi criado corretamente!")
+
 
 q8 = questao8
 
 class questao9:
   def solution():
-    print(print(df['tipoAutor'].value_counts()[:4]))
+    print("print(df['tipoAutor'].value_counts()[:4])")
   def hint():
     print("Utilize o método series.value_count() em 'tipoAutor'.")
   def check():
@@ -169,9 +195,14 @@ print(max_partido)""")
   def hint():
     print("Selecione os 10 primeiros itens do método series.value_count() da coluna 'siglaPartidoAutor'.")
   def check():
-    if(str(max_partidos_teste) == str(max_partidos)):
-      print("\033[92mCorreto\033[0m: Parabéns, você finalizou o notebook!")
-    else:
+    try:
+      if(str(max_partidos_teste) == str(max_partidos)):
+        print("\033[92mCorreto\033[0m: Parabéns, você finalizou o notebook!")
+      else:
+        print("\033[91mErrado\033[0m: max_partidos não foi criado corretamente!")
+    except ValueError:
       print("\033[91mErrado\033[0m: max_partidos não foi criado corretamente!")
+      
+q10 = questao10
 
 print("\033[1mNotebook pronto para uso\033[0m")
